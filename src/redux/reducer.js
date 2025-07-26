c3e0a42fc3b049e2d0e583f864dba702d7488e03
tewa92 <tewachewsaleigizer@gmail.com>
2025-07-26 17:08:19 +0300

    Switch Statement in Reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,19 @@
 import {ADD_TO_CART} from './constant'
 
 export const cartData = (data=[], action) =>{
-	if (action.type === ADD_TO_CART){
-		console.warn("ADD_TO_CART reducer called!", action);
+	// if (action.type === ADD_TO_CART){
+	// 	console.warn("ADD_TO_CART reducer called!", action);
 
-		return data
-	} else {
-		return "no action called"
+	// 	return data
+	// } else {
+	// 	return "no action called"
+	// }
+
+	switch(action.type){
+		case ADD_TO_CART:
+			console.warn("ADD_TO_CART reducer called!", action);
+			return 1+1
+		default:
+			return "no action called"
 	}
-	return 10
 }
