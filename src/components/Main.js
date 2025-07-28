ccb0ea8d57a62ed409928aa69049be19e7946a0e
tewa92 <tewachewsaleigizer@gmail.com>
2025-07-28 16:06:41 +0300

    product list ui

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,91 +1,48 @@
 import {addToCart, removeFromCart, emptyCart} from '../redux/action';
 import {useDispatch, useSelector} from 'react-redux';
 import {productList} from '../redux/productAction'
+import { useEffect } from 'react';
+
 
 function Main() {
   const dispatch = useDispatch();
   let data = useSelector((state) => state.productData);
   console.warn("data in main component", data);
 
-  // const product = {
-  //   name: "i-phone",
-  //   category: "mobile",
-  //   price: "1000",
-  // }
 
-  const product = [
-  {
-    name: "i-phone",
-    category: "mobile",
-    price: "1000",
-  },
-  {
-    name: "macbook pro",
-    category: "laptop",
-    price: "2000",
-  },
-  {
-    name: "galaxy s22",
-    category: "mobile",
-    price: "850",
-  },
-  {
-    name: "ipad air",
-    category: "tablet",
-    price: "700",
-  },
-  {
-    name: "airpods pro",
-    category: "accessory",
-    price: "250",
-  },
-  {
-    name: "apple watch",
-    category: "wearable",
-    price: "400",
-  },
-  {
-    name: "sony headphones",
-    category: "accessory",
-    price: "180",
-  },
-  {
-    name: "dell xps 13",
-    category: "laptop",
-    price: "1500",
-  },
-  {
-    name: "samsung smart tv",
-    category: "home electronics",
-    price: "1200",
-  },
-  {
-    name: "google pixel 7",
-    category: "mobile",
-    price: "799",
-  }
-];
+useEffect(()=> {
+  dispatch(productList())
+}, [])
 
   return (
     <div>
-      <button onClick={() =>dispatch(addToCart(product))}>
-        Add to cart
-      </button>
-        <div>
-          <button onClick={() =>dispatch(removeFromCart(product.name))}>
-            Remove from cart
-          </button>
-        </div>
         <div>
           <button onClick={() =>dispatch(emptyCart())}>
             Empty cart
           </button>
         </div>
        <div>
-        <button onClick={() =>dispatch(productList())}>
-          Product List
-        </button>
       </div>
+
+      <div className="product-container">
+      {
+        data.map((item)=> (
+        <div key={item.id} className="product-item">
+          <img src={item.imgUrl} alt=""/>
+          <div>Name: {item.name}</div>
+          <div>Color: {item.color}</div>
+          <div>Price: {item.price}</div>
+          <div>Category: {item.category}</div>
+          <div>Brand: {item.brand}</div>
+          <div>
+          <button onClick={() =>dispatch(addToCart(item))} >Add to cart</button>
+          <button onClick={() =>dispatch(removeFromCart(item.id))}>Remove to cart</button>
+          </div>
+
+        </div>
+        ))
+      }
+     </div>
 
     </div>
   );
