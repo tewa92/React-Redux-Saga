8518b76b3093bbb1d743a38ded2baf011d480b1f
tewa92 <tewachewsaleigizer@gmail.com>
2025-07-28 16:19:09 +0300

    add functionality for remove item from cart

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -29,26 +29,22 @@
 
 import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from './constant';
 
-export const cartData = (state = [], action) => {
+export const cartData = (data = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
       console.warn("ADD_TO_CART reducer called!", action);
-      return [action.data, ...state]; // Add item to beginning
+      return [action.data, ...data]; // Add item to beginning
 
     case REMOVE_FROM_CART:
       console.warn("REMOVE_FROM_CART reducer called!", action);
-      const indexToRemove = state.findIndex(item => item.name === action.data);
-      if (indexToRemove === -1) return state;
-      return [
-        ...state.slice(0, indexToRemove),
-        ...state.slice(indexToRemove + 1)
-      ];
-
+      const remaingItems = data.filter((item) =>item.id !== action.data);
+      return [...remaingItems]
     case EMPTY_CART:
       console.warn("EMPTY_CART reducer called!", action);
-      return []; // Clear cart immutably
+      data = []
+      return [...data]; // Clear cart immutably
 
     default:
-      return state;
+      return data;
   }
 };
